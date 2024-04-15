92967fc979846153f6834c1c99ace5192b7508a5
michaellinsk <michael.linsk01@gmail.com>
2024-04-14 23:05:21 -0300

    commit final

--- index.php
+++ index.php
@@ -14,26 +14,41 @@ $ultimoAcesso = mysqli_fetch_assoc($resultado)
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Informações sobre Mortes por Covid</title>
-  <link rel="stylesheet" href="estilo.css">
+  <title>Informações sobre Mortes por Covid </title>
+  <link rel="stylesheet" href="style.css">
 </head>
-  <h1>Dados de Mortes por Covid</h1>
+<body>
+  <header>
+  <h1>Consulte Covid</h1>
+</header>
+<h2> Consulte os registros de casos de Covid19 de cada País</h2>
+<form>
+  <div>
+    <img src="./img/australia.png" alt= "Bandeira da australia">
+      <input type="radio" id="australia" name="pais" value="Australia">
+      <label for="australia">Austrália</label><br>
+  </div>
 
-  <form>
-  <input type="radio" id="australia" name="pais" value="Australia">
-  <label for="australia">Austrália</label><br>
-  <input type="radio" id="brasil" name="pais" value="Brazil">
-  <label for="brasil">Brasil</label><br>
-  <input type="radio" id="canadá" name="pais" value="Canadá">
-  <label for="venezuela">Canadá</label><br>
+  <div>
+    <img src="./img/brasil.jpeg" alt= "Bandeira da australia">
+    <input type="radio" id="brasil" name="pais" value="Brazil">
+    <label for="brasil">Brasil</label><br>
+  </div>
+
+  <div>
+    <img src="./img/canada.jpeg" alt= "Bandeira da australia">
+    <input type="radio" id="canadá" name="pais" value="Canadá">
+    <label for="venezuela">Canadá</label><br>
+  </div>
+
 </form>
 
 <button id="buscarDados">Buscar Dados</button>
 
 <div id="dadosPais">
-  Selecione um país e clique em "Buscar Dados" para ver os dados COVID-19.
-</div>
 
+</div>
+<br>
 <div id="totalConfirmados">
    Casos Confirmados:
 </div>
@@ -41,8 +56,13 @@ $ultimoAcesso = mysqli_fetch_assoc($resultado)
     Total de Mortes:
     <p id="ultimaConsulta"></p>
 </div>
- <p>Última consulta: <?php  echo $ultimoAcesso['pais'] . " - " . $ultimoAcesso['data_hora']; ?></p>
-</div>
+    
+  <footer>
+  
+      <p>Última consulta: <?php  echo $ultimoAcesso['pais'] . " - " . $ultimoAcesso['data_hora']; ?></p>
+ 
+
+</footer>
 
 <script src= "script.js"></script>
 
---
+++ readme.md
@@ -0,0 +1,46 @@
+# Consulte Covid - Documentação do Projeto
+
+Este é um projeto simples de uma aplicação web para consultar dados sobre casos de COVID-19 em diferentes países.
+
+## Conteúdo do Repositório
+
+O repositório contém os seguintes arquivos:
+
+- `index.php`: Arquivo principal da aplicação web.
+- `style.css`: Arquivo de estilo para estilizar a página HTML.
+- `script.js`: Arquivo de script JavaScript para interatividade na página.
+- `conexao.php`: Arquivo PHP para estabelecer a conexão com o banco de dados.
+- `Banco de dados MySQL`: Um banco de dados MySQL para receber a data e hora da ultimaconsulta.
+
+## Como Executar
+
+Para executar a aplicação, siga os passos abaixo:
+
+1. Clone o repositório para o seu ambiente local.
+2. Certifique-se de ter um servidor web (como Apache) e PHP instalados no seu sistema.
+3. Coloque os arquivos no diretório raiz do seu servidor web.
+4. Inicie o servidor web.
+5. Acesse a página `index.php` no seu navegador.
+
+## Funcionalidades
+
+A aplicação permite:
+
+- Selecionar um país para consultar os dados de COVID-19.
+- Visualizar os casos confirmados e mortes por COVID-19 no país selecionado.
+- Enviar os dados do país para um backend.
+
+## Contribuindo
+
+Se deseja contribuir com este projeto, siga estas etapas:
+
+1. Faça um fork do repositório.
+2. Crie uma branch para a sua feature (`git checkout -b feature/SuaFeature`).
+3. Faça commit das suas alterações (`git commit -am 'Adiciona nova feature'`).
+4. Faça push para a branch (`git push origin feature/SuaFeature`).
+5. Abra um Pull Request.
+
+## Autor
+
+[MICHAEL RODRIGUES] - [michael.linsak01@gmail.com]
+
